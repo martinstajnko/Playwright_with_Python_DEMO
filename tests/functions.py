3055f849ef19067af6ae152432cb5aca5b9be6ea
martinstajnko <stajnko.martin93@gmail.com>
2023-05-31 22:09:44 +0200

    modification

--- tests/functions.py
+++ tests/functions.py
@@ -3,14 +3,21 @@ from time import sleep
 from dotenv import dotenv_values
 from playwright.sync_api import expect
 
+
+ENV_VALUES = dotenv_values('secrets.env')
+
 class WooCommerceBackoffice:
 
-    env_values = dotenv_values('secrets.env')
-
     def login_to_backoffice(self, page):
-        page.goto(self.env_values['WOOCOMMERCE_BACKOFFICE_URL'])
-        page.fill('#user_login', self.env_values['WOOCOMMERCE_BACKOFFICE_USERNAME'])
-        page.fill('#user_pass', self.env_values['WOOCOMMERCE_BACKOFFICE_PASSWORD'])
+        page.goto(ENV_VALUES['WOOCOMMERCE_BACKOFFICE_URL'])
+        page.fill('#user_login', ENV_VALUES['WOOCOMMERCE_BACKOFFICE_USERNAME'])
+        page.fill('#user_pass', ENV_VALUES['WOOCOMMERCE_BACKOFFICE_PASSWORD'])
         page.click('#wp-submit')
         expect(page).to_have_title('Dashboard ‹ WeGetFinancing Demo Website — WordPress')
         sleep(0.5)
+
+
+
+class WooCommerceStore:
+
+    pass
